2786c24eaa96d8dc5f1fc2edee8b974b5af9b6a1
도준 <kosae05@gmail.com>
2019-07-07 23:24:50 +0900

    회원가입 생성중

--- app/src/main/java/com/example/hanieum/Communication.java
+++ app/src/main/java/com/example/hanieum/Communication.java
@@ -1,7 +1,10 @@
 package com.example.hanieum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Communication extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class Communication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
+
+        Button bt_VoiceCall = findViewById(R.id.VoiceCall);
+        bt_VoiceCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VoiceCall.class);
+                startActivity(intent);
+            }
+        });
+
+        Button bt_VideoCall = findViewById(R.id.VideoCall);
+        bt_VideoCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VideoCall.class);
+                startActivity(intent);
+            }
+        });
+        Button bt_Message = findViewById(R.id.Message);
+        bt_Message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Message.class);
+                startActivity(intent);
+            }
+        });
+        Button bt_Cloud = findViewById(R.id.Cloud);
+        bt_Cloud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Cloud.class);
+                startActivity(intent);
+            }
+        });
     }
 }
